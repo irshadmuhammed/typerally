452cd7d6eaac6dbb2c62670723ce92f0b5582248
Muhammed Irshad M <irshadmm16@gmail.com>
2023-02-28 21:37:16 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/typerally/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.typerally
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/create_room_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+import 'package:typerally/utils/socket_client.dart';
+import 'package:typerally/utils/socket_method.dart';
+import 'package:typerally/widgets/custom_button.dart';
+import 'package:typerally/widgets/custom_textfield.dart';
+
+class CreateRoomScreen extends StatefulWidget {
+  const CreateRoomScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreateRoomScreen> createState() => _CreateRoomScreenState();
+}
+
+class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final SocketMethods _socketMethods = SocketMethods();
+
+  @override
+  void initState() {
+    super.initState();
+    _socketMethods.updateGameListener(context);
+    _socketMethods.notCorrectGameListener(context);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: 600,
+          ),
+          child: Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Create Room',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.08,
+                ),
+                CustomTextField(
+                  controller: _nameController,
+                  hintText: 'Enter your nickname',
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomButton(
+                  text: 'Create',
+                  onTap: () => _socketMethods.createGame(
+                    _nameController.text,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
